4487e89dfa8fe6cd7972e39ff6cefb2a6f88e538
Mahesh Kumawat <maheshkumawat0911@gmail.com>
2020-08-14 18:57:09 +0530

    Version 7

---
+++ v7/routes/comments.js
@@ -0,0 +1,48 @@
+var express = require("express");
+var router = express.Router({mergeParams:true});
+var Campground = require("../models/campground");
+var Comment = require('../models/comment');
+
+router.get("/new",isLoggedIn,function(req,res){
+    Campground.findById(req.params.id,function(err,campground){
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.render("comments/new",{campground:campground});
+        }
+    })
+});
+
+router.post("/",function(req,res){
+    Campground.findById(req.params.id,function(err,campground){
+        if(err){
+            console.log(err);
+            res.redirect("/campgrounds");
+        }
+        else{
+            Comment.create(req.body.comment,function(err,comment){
+                if(err){
+                    console.log(err);
+                }
+                else{
+                    comment.author.id = req.user._id;
+                    comment.author.username = req.user.username;
+                    comment.save();
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect("/campgrounds/"+campground._id);
+                }
+            })
+        }
+    })
+});
+
+function isLoggedIn(req,res,next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
+
+module.exports = router;
